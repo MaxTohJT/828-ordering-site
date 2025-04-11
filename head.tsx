65f3eeb1dfb0751d7de421ac3c352ef9c05a7b15
MaxTohJT <maxtoh262808@gmail.com>
2025-04-11 12:36:28 +0800

    Add files via upload

---
+++ head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>828 Wuxiang & Satay</title>
+      <meta name="description" content="Order satay online from 828 Wuxiang & Satay" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+}
