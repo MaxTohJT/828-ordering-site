25349874a22b63ece70a56e6519296c072b6d7ec
MaxTohJT <maxtoh262808@gmail.com>
2025-04-11 12:54:20 +0800

    Delete app/head.tsx

--- app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>828 Wuxiang & Satay</title>
-      <meta name="description" content="Order satay online from 828 Wuxiang & Satay" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </>
-  );
-}
